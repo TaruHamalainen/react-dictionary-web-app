a1b3a4e532dfb9dd0cdf1b0d2111f57c8fe55069
Taru Hämäläinen <82812247+TaruHamalainen@users.noreply.github.com>
2023-11-13 20:09:59 +0200

    storing and getting user settings from localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import NavBar from "./components/NavBar";
 
 export default function App() {
-  const [userSettings, setUserSettings] = useState({
-    font: "Sans Serif",
-    theme: "light",
+  // Usersettings to change font and color theme
+  const [userSettings, setUserSettings] = useState(() => {
+    const savedUserSettings = localStorage.getItem("userSettings");
+    return savedUserSettings
+      ? JSON.parse(savedUserSettings)
+      : {
+          font: "Sans Serif",
+          theme: "light",
+        };
   });
 
+  // saving user settings to local storage
+  useEffect(() => {
+    localStorage.setItem("userSettings", JSON.stringify(userSettings));
+  }, [userSettings]);
+
+  // Reference to font selection pop up menu
   let popUpRef = useRef(null);
 
+  // changing theme
   const handleThemeChange = () => {
     setUserSettings({
       ...userSettings,
